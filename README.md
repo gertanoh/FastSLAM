a456493fb7cffcae2b45ec1c4a9692109ed27580
thenryci1996 <thenry702@gmail.com>
2018-11-24 11:44:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# FastSLAM #
+------------------------------------------------------------------------------------
+
+
+## Python Implementation of FastSLAM 1 and 2 ##
+
+
+## Source ##
+The code was implemented after following lectures on Introduction to Mobile
+Robotics at 
+[University of Freiburg](http://ais.informatik.uni-freiburg.de/teaching/ss18/robotics/index_en.php)
+* The data and some parts of the code are available on the link described above.
+* For FastSLAM 2, [Paper](http://robots.stanford.edu/papers/Montemerlo03a.pdf)
---
+++ code/fastslam_2.py
@@ -0,0 +1,345 @@
+from read_data import read_world, read_sensor_data
+from misc_tools import *
+import numpy as np
+import numpy.linalg.inv as inv
+import math
+import copy
+
+#plot preferences, interactive plotting mode
+plt.axis([-1, 12, 0, 10])
+plt.ion()
+plt.show()
+
+def initialize_particles(num_particles, num_landmarks):
+    #initialize particle at pose [0,0,0] with an empty map
+
+    particles = []
+
+    for i in range(num_particles):
+        particle = dict()
+
+        #initialize pose: at the beginning, robot is certain it is at [0,0,0]
+        particle['x'] = 0
+        particle['y'] = 0
+        particle['theta'] = 0
+
+        #initial weight
+        particle['weight'] = 1.0 / num_particles
+        
+        #particle history aka all visited poses
+        particle['history'] = []
+
+        #initialize landmarks of the particle
+        landmarks = dict()
+
+        for i in range(num_landmarks):
+            landmark = dict()
+
+            #initialize the landmark mean and covariance 
+            landmark['mu'] = [0,0]
+            landmark['sigma'] = np.zeros([2,2])
+            landmark['observed'] = False
+
+            landmarks[i+1] = landmark
+
+            #add landmarks to particle
+            particle['landmarks'] = landmarks
+
+            #add particle to set
+            particles.append(particle)
+
+    return particles
+
+
+def measurement_model(particle, landmark):
+    #Compute the expected measurement for a landmark
+    #and the Jacobian with respect to the landmark.
+
+    px = particle['x']
+    py = particle['y']
+    ptheta = particle['theta']
+
+    lx = landmark['mu'][0]
+    ly = landmark['mu'][1]
+    
+    diff_x = (lx - px)
+    diff_y = (ly - py)
+    
+
+    #calculate expected range measurement
+    meas_range_exp = np.sqrt( diff_x**2 + diff_y**2 )
+    meas_bearing_exp = math.atan2(diff_y, diff_x) - ptheta
+
+    h = np.array([meas_range_exp, meas_bearing_exp])
+    h2 = h[0]**2
+  
+    # Compute the Jacobian H of the measurement function h 
+    #wrt the landmark location. Range only sensor
+    
+    H = np.zeros((2,2))
+    H[0,0] = diff_x / h[0]
+    H[0,1] = diff_y / h[0]
+    H[1,0] = -diff_y / h2
+    H[1,1] = diff_x / h2
+    
+    H_pose = np.zeros((2,3))
+    H_pose[0,0] = -diff_x / h[0]
+    H_pose[0,1] = -diff_y / h[0]
+    H_pose[0,2] = 0
+    
+    H_pose[1,0] = diff_y / h2
+    H_pose[1,1] = -diff_x / h2
+    H_pose[1,1] = -1
+
+    return h, H, H_pose
+
+
+
+def sample_motion_model(odometry, particles):
+    
+    
+  
+    # Updates the particle positions, based on old positions, the odometry
+    # measurements and the motion noise 
+    delta_rot1 = odometry['r1']
+    delta_trans = odometry['t']
+    delta_rot2 = odometry['r2']
+
+    # the motion noise parameters: [alpha1, alpha2, alpha3, alpha4]
+    noise = [0.1, 0.1, 0.05, 0.05]
+    
+
+    sigma_delta_rot1 = noise[0] * abs(delta_rot1) + noise[1] * delta_trans
+    
+    sigma_delta_trans = noise[2] * delta_trans + noise[3] * (abs(delta_rot1) + abs(delta_rot2))
+    
+    
+    sigma_delta_rot2 = noise[0] * abs(delta_rot2) + noise[1] * delta_trans
+    
+    # "move" each particle according to the odometry measurements plus sampled noise
+    
+    for particle in particles:
+        
+                
+        noisy_delta_rot1 = delta_rot1 + np.random.normal(0, sigma_delta_rot1)
+    
+        noisy_delta_trans = delta_trans + np.random.normal(0, sigma_delta_trans)
+        noisy_delta_rot2 = delta_rot2 + np.random.normal(0, sigma_delta_rot2)
+        
+        
+        particle['history'].append([particle['x'], particle['y']])
+        
+        
+        particle['x'] = particle['x'] + noisy_delta_trans * np.cos(particle['theta'] + noisy_delta_rot1)
+        particle['y'] = particle['y'] + noisy_delta_trans * np.sin(particle['theta'] + noisy_delta_rot1)
+        particle['theta'] = particle['theta'] + noisy_delta_rot1 + noisy_delta_rot2
+        
+
+
+     
+    return np.array([[sigma_delta_trans, 0, 0],
+                    [0, sigma_delta_trans, 0],
+                    [0, 0, sigma_delta_rot2]]
+
+
+
+def eval_sensor_model(sensor_data, particles, process_noise):
+    
+        
+    #Correct landmark poses with a measurement and
+    #calculate particle weight
+
+    #sensor noise
+    Q_t = np.array([[0.1, 0],\
+            [0, 0.1]])
+    
+
+    #measured landmark ids and ranges
+    ids = sensor_data['id']
+    ranges = sensor_data['range']
+    bearings = sensor_data['bearing']
+
+    #update landmarks and calculate weight for each particle
+    for particle in particles:
+        
+        landmarks = particle['landmarks']
+
+        px = particle['x']
+        py = particle['y']
+        ptheta = particle['theta'] 
+        pose = np.array([[px], [py], [ptheta]])
+        
+
+        #loop over observed landmarks 
+        for i in range(len(ids)):
+
+            #current landmark
+            lm_id = ids[i]
+            landmark = landmarks[lm_id]
+            
+            #measured range and bearing to current landmark
+            meas_range = ranges[i]
+            meas_bearing = bearings[i]
+
+            if not landmark['observed']:
+                
+                # landmark is observed for the first time
+                
+                # initialize landmark mean and covariance. You can use the
+                # provided function 'measurement_model' above
+
+                l_x = px + meas_range * np.cos(ptheta + meas_bearing)
+                l_y = py + meas_range * np.sin(ptheta + meas_bearing)
+                landmark['mu'] = [l_x, l_y]
+                
+                h, H = measurement_model(particle, landmark)
+                try:
+                    H_inv = np.linalg.inv(H)
+                    landmark['sigma'] = H_inv @ Q_t @ H_inv.transpose()
+                except:
+                    print('Error: H inverse does not exist')
+                
+                landmark['observed'] = True
+
+            else:
+                # landmark was observed before
+                
+                h, H, H_pose = measurement_model(particle, landmark) 
+                y_k = np.array([meas_range - h[0], angle_diff(meas_bearing, h[1]) ])
+                
+                
+                # update sample distribution
+                # use landmark covariance of previous step
+                Qt = Q_t + H @ landmark['sigma'] @ H.transpose()
+                pose_cov = inv.(H_pose.transpose() @ inv(Qt) @ H_pose + inv(process_noise))
+                pose_mean = pose_cov @ H_pose.transpose() @ inv(Qt) @ y_k + pose
+                particle['x'] = pose_mean[0]
+                particle['y'] = pose_mean[1]
+                particle['theta'] = pose_mean[2]                        
+
+                # update landmark mean and covariance. You can use the
+                # provided function 'measurement_model' above. 
+                # calculate particle weight: particle['weight'] = ...
+
+                
+                cov = landmark['sigma']
+                S = H @ cov @ H.transpose() + Q_t
+                K = cov @ H.transpose() @ np.linalg.inv(S)
+                
+                landmark['mu'] = landmark['mu'] + (K @ y_k)
+                landmark['sigma'] = (np.identity(2) - (K @ H)) @ cov
+                
+                # Likelyhood of observation
+                w_k = np.exp((-.5)*(y_k.transpose() @ np.linalg.inv(S) @ y_k))
+                weight = w_k / np.sqrt( ((2*math.pi)**2) * np.linalg.det(S))
+
+                particle['weight'] *= weight
+            
+    #normalize weights
+    normalizer = sum([p['weight'] for p in particles])
+
+    for particle in particles:
+        particle['weight'] = particle['weight'] / normalizer
+        
+    
+
+def resample_particles(particles):
+    # Returns a new set of particles obtained by performing
+    # stochastic universal sampling, according to the particle 
+    # weights.
+
+    new_particles = []
+    
+    
+    # distance between pointers
+    step = 1.0 / len(particles)
+    
+    # random start of first pointer 
+    p = np.random.uniform(0, step)
+    
+    # Position along the weights
+    c = particles[0]['weight']
+    
+    # index 
+    i = 0
+    
+    # Draw sample 
+    for particle in particles:
+        # loop until i is the particle
+        # p points to
+        while p > c:
+            i = i + 1
+            c = c + particles[i]['weight']
+            
+        new_particle = copy.deepcopy(particles[i])
+        new_particle['weight'] = step
+        new_particles.append(new_particle)
+        
+        # increase threshold
+        p = p + step
+    
+    return new_particles
+
+
+def RMSE(bestParticleLandmarks, landmarks):
+  
+        rmse = 0.0
+        index = 1
+  
+        for key, landmark in bestParticleLandmarks.items():  
+                 diff = landmark['mu'] - landmarks[index]
+                 index = index + 1
+    
+                 diff = diff * diff    
+                 rmse = rmse + diff
+  
+        rmse = rmse / len(landmarks)
+        rmse = np.sqrt(rmse)
+        
+        return rmse
+
+def mainPA():
+
+  
+  
+    print("--- efve Reading landmark positions")
+    landmarks = read_world("../data/world.dat")
+
+    print("Reading sensor data")
+    sensor_readings = read_sensor_data("../data/sensor_data.dat")
+
+    num_particles = 10
+    num_landmarks = len(landmarks)
+
+    #create particle set
+    particles = initialize_particles(num_particles, num_landmarks)
+    
+    bestParticle = None
+
+    #run FastSLAM
+    for timestep in range(int(len(sensor_readings)/2)):
+
+        #predict particles by sampling from motion model with odometry info
+        process_noise = sample_motion_model(sensor_readings[timestep,'odometry'], particles)
+
+        #evaluate sensor model to update landmarks and calculate particle weights
+        eval_sensor_model(sensor_readings[timestep, 'sensor'], particles, process_noise)
+
+        #plot filter state
+        #plot_state(particles, landmarks)
+        
+        bestParticle = best_particle(particles)
+        #calculate new set of equally weighted particles
+        particles = resample_particles(particles)
+  
+ 
+    #plt.show()
+    #print('real word data landmarks ')
+    #print(landmarks)
+    #print('Best particle estimate ')
+    #print(bestParticle['landmarks'])
+    rmse = RMSE(bestParticle['landmarks'], landmarks)
+    print('rmse : ', rmse)
+
+if __name__ == "__main__":
+    mainPA()
